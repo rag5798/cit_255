011f16db89ff61872d29d2f6cfcab1b4e0f8b7c2
rag5798 <bgoettman@yahoo.com>
2024-03-13 23:15:15 -0600

    almost all inserts of tables

--- almost_all_inserts.sql
+++ almost_all_inserts.sql
@@ -34,7 +34,7 @@ DROP TABLE IF EXISTS `flower_shop`.`address` ;
 
 CREATE TABLE IF NOT EXISTS `flower_shop`.`address` (
   `address_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `house_number` VARCHAR(45) NOT NULL,
+  `house_number` VARCHAR(45) NULL,
   `street_name` VARCHAR(45) NOT NULL,
   `city` VARCHAR(45) BINARY NOT NULL,
   `state_id` INT UNSIGNED NOT NULL,
@@ -146,7 +146,7 @@ CREATE TABLE IF NOT EXISTS `flower_shop`.`order` (
   `store_id` INT UNSIGNED NOT NULL,
   `employee_id` INT UNSIGNED NOT NULL,
   `customer_id` INT UNSIGNED NOT NULL,
-  `date` DATETIME NOT NULL,
+  `date` DATE NOT NULL,
   `total` DOUBLE(10,2) UNSIGNED NULL,
   PRIMARY KEY (`order_id`),
   INDEX `fk_transaction_customer1_idx` (`customer_id` ASC) VISIBLE,
@@ -323,12 +323,12 @@ CREATE TABLE IF NOT EXISTS `flower_shop`.`store_employee` (
   PRIMARY KEY (`store_employee_id`),
   INDEX `fk_store_employee_employee1_idx` (`employee_id` ASC) VISIBLE,
   INDEX `fk_store_employee_store1_idx` (`store_id` ASC) VISIBLE,
-  CONSTRAINT `fk_store_employee_employee1`
+  CONSTRAINT `store_employee_fk1`
     FOREIGN KEY (`employee_id`)
     REFERENCES `flower_shop`.`employee` (`employee_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `fk_store_employee_store1`
+  CONSTRAINT `store_employee_fk2`
     FOREIGN KEY (`store_id`)
     REFERENCES `flower_shop`.`store` (`store_id`)
     ON DELETE NO ACTION
@@ -385,37 +385,201 @@ SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
 
 
+
 use flower_shop;
 -- ------------------------------------------------ -- 
 --    Needs Inserts put in as of March 08 2024      --
 -- ------------------------------------------------- -- 
-SELECT * FROM delivery; -- still needs to insert
-SELECT * FROM discount; -- Still needs to insert
-SELECT * FROM inventory; -- Still need to insert 
-SELECT * FROM item_type; -- Still need to insert
-SELECT * FROM occation;-- Still needs insert
-SELECT * FROM order_line; -- Still needs insert 
-SELECT * FROM product; -- Still need to insert
-SELECT * FROM store_employee; -- Still need to insert 
-SELECT * FROM truck; -- Still need to insert
-
+-- SELECT * FROM delivery; -- still needs to insert
+-- SELECT * FROM discount; -- Still needs to insert
+-- SELECT * FROM inventory; -- Still need to insert 
+-- SELECT * FROM item_type; -- Still need to insert
+-- SELECT * FROM occation;-- Still needs insert
+-- SELECT * FROM order_line; -- Still needs insert 
+-- SELECT * FROM product; -- Still need to insert
+-- SELECT * FROM store_employee; -- Still need to insert 
+-- SELECT * FROM truck; -- Still need to insert
 
 
 
 -- --------------------------------------------------- --
 -- 		double check the table name is okay to use	   --
 -- --------------------------------------------------- --
-SELECT * FROM role;
+-- SELECT * FROM role;
 
 -- -------------- --
 -- Tables work --
 -- -------------- --
-SELECT * FROM employee;
+-- SELECT * FROM employee;
 
 
 -- -------------------
 -- Playing around --
--- -------------- --
+-- -------------- –
+
+-- —------------------------
+-- Cayleigh's attempt at inserts —--
+-- —------------------------------
+SELECT * FROM discount;
+
+
+
+
 
 SELECT * FROM state;
-INSERT INTO state VALUES ()
+INSERT INTO state VALUES (default, 'PA');
+
+INSERT INTO state VALUES (default, 'ID');
+
+INSERT INTO state VALUES (default, 'AZ');
+
+
+
+SELECT * FROM address;
+INSERT INTO address VALUES (default, 36, 'Random Street', 'Pittsburgh', (SELECT state_id FROM state WHERE state_abv = 'PA'), '15009');
+
+INSERT INTO address VALUES (default, 6, 'Anonymous Avenue', 'Rexburgh', (SELECT state_id FROM state WHERE state_abv = 'ID'), '84430');
+
+INSERT INTO address VALUES (default, 6, 'Default Boulevard', 'Phoenix', (SELECT state_id FROM state WHERE state_abv = 'AZ'), '85001');
+
+INSERT INTO address VALUES (default, 6, 'Big Boulevard', 'Rexburgh', (SELECT state_id FROM state WHERE state_abv = 'ID'), '84430');
+
+INSERT INTO address VALUES (default, 6, 'Google Street', 'Phoenix', (SELECT state_id FROM state WHERE state_abv = 'AZ'), '85001');
+
+INSERT INTO address VALUES (default, 6, 'Clemente Street', 'Pittsburgh', (SELECT state_id FROM state WHERE state_abv = 'PA'), '85001');
+
+INSERT INTO address VALUES (default, '2802', 'Johnathan Junction', 'Alexville', (SELECT state_id FROM state WHERE state_abv = 'PA'),'18555');
+
+INSERT INTO address VALUES (default, '26497', 'Angela Walk', 'North Cole', (SELECT state_id FROM state WHERE state_abv = 'ID'),'79892');
+
+INSERT INTO address VALUES (default, '2648', 'Laura Prairie', 'Gonzalezchester', (SELECT state_id FROM state WHERE state_abv = 'AZ'),'46796');
+
+
+
+SELECT * FROM store;
+INSERT INTO store VALUES (default, '724-890-9190', (SELECT address_id FROM address WHERE city = 'Rexburgh' and street_name = 'Anonymous Avenue'));
+
+INSERT INTO store VALUES (default, '800-192-7392', (SELECT address_id FROM address WHERE city = 'Phoenix' and street_name = 'Default Boulevard'));
+
+INSERT INTO store VALUES (default, '724-890-9190', (SELECT address_id FROM address WHERE city = 'Pittsburgh' and street_name = 'Random Street'));
+
+
+
+
+SELECT * FROM customer;
+INSERT INTO customer (first_name, last_name, store_id, address_id) VALUES ('Emily', 'Levy', (SELECT store_id FROM store s LEFT OUTER JOIN address a ON a.address_id = s.address_id WHERE city = 'Pittsburgh'), (SELECT address_id FROM address WHERE city = 'Pittsburgh' and street_name = 'Clemente Street'));
+
+INSERT INTO customer (first_name, last_name, store_id, address_id) VALUES ('Douglas', 'Young', (SELECT store_id FROM store s LEFT OUTER JOIN address a ON a.address_id = s.address_id WHERE city = 'Phoenix'), (SELECT address_id FROM address WHERE city = 'Phoenix' and street_name = 'Google Street'));
+
+
+INSERT INTO customer (first_name, last_name, store_id, address_id) VALUES ('Cesar', 'Moyer', (SELECT store_id FROM store s LEFT OUTER JOIN address a ON a.address_id = s.address_id WHERE city = 'Rexburgh'),(SELECT address_id FROM address WHERE city = 'Rexburgh' and street_name = 'Big Boulevard'));
+
+
+
+
+SELECT * FROM role;
+INSERT INTO role (role_name) VALUES ('Cashier');
+
+INSERT INTO role (role_name) VALUES ('Manager');
+
+INSERT INTO role (role_name) VALUES ('Floral Designer');
+
+
+
+
+SELECT * FROM employee;
+INSERT INTO employee (first_name, last_name, role_id, start_date, end_date, address_id)
+VALUES ('Heather', 'Washington', (SELECT role_id FROM role WHERE role_name = 'Cashier'), '2022-06-10', '2022-09-23', (SELECT address_id FROM address WHERE city = 'Alexville' and street_name = 'Johnathan Junction'));
+
+
+INSERT INTO employee (first_name, last_name, role_id, start_date, end_date, address_id)
+VALUES ('Alex', 'Jackson', (SELECT role_id FROM role WHERE role_name = 'Manager'), '2023-11-28', '2024-11-09', (SELECT address_id FROM address WHERE city = 'North Cole' and street_name = 'Angela Walk'));
+
+
+INSERT INTO employee (first_name, last_name, role_id, start_date, end_date, address_id)
+VALUES ('Jason', 'Murray', (SELECT role_id FROM role WHERE role_name = 'Floral Designer'), '2015-01-02', '2015-10-15', (SELECT address_id FROM address WHERE city = 'Gonzalezchester' and street_name = 'Laura Prairie'));
+
+
+SELECT * FROM store_employee;
+INSERT INTO store_employee (employee_id, store_id) VALUES ((SELECT employee_id FROM employee WHERE first_name = 'Heather' and last_name = 'Washington' and role_id = (SELECT role_id FROM role WHERE role_name = 'Cashier')), (SELECT store_id FROM store s INNER JOIN address a ON a.address_id = s.address_id INNER JOIN state st ON st.state_id = a.state_id WHERE state_abv = (SELECT state_abv FROM employee e INNER JOIN address a ON e.address_id = a.address_id INNER JOIN state st ON st.state_id = a.state_id WHERE e.first_name = 'Heather')));
+
+INSERT INTO store_employee (employee_id, store_id) VALUES ((SELECT employee_id FROM employee WHERE first_name = 'Alex' and last_name = 'Jackson' and role_id = (SELECT role_id FROM role WHERE role_name = 'Manager')), (SELECT store_id FROM store s INNER JOIN address a ON a.address_id = s.address_id INNER JOIN state st ON st.state_id = a.state_id WHERE state_abv = (SELECT state_abv FROM employee e INNER JOIN address a ON e.address_id = a.address_id INNER JOIN state st ON st.state_id = a.state_id WHERE e.first_name = 'Alex')));
+
+INSERT INTO store_employee (employee_id, store_id) VALUES ((SELECT employee_id FROM employee WHERE first_name = 'Jason' and last_name = 'Murray' and role_id = (SELECT role_id FROM role WHERE role_name = 'Floral Designer')), (SELECT store_id FROM store s INNER JOIN address a ON a.address_id = s.address_id INNER JOIN state st ON st.state_id = a.state_id WHERE state_abv = (SELECT state_abv FROM employee e INNER JOIN address a ON e.address_id = a.address_id INNER JOIN state st ON st.state_id = a.state_id WHERE e.first_name = 'Jason')));
+
+
+
+
+SELECT * FROM occation;
+INSERT INTO occation (occation_name) VALUES ('Birthday');
+
+INSERT INTO occation (occation_name) VALUES ('Valintine');
+
+INSERT INTO occation (occation_name) VALUES ('Anniversary');
+
+
+
+
+SELECT * FROM item_type;
+INSERT INTO item_type (item_name) VALUES ('Flower');
+
+INSERT INTO item_type (item_name) VALUES ('Ballon');
+
+INSERT INTO item_type (item_name) VALUES ('Valintine\'s Bouquet');
+
+
+
+
+SELECT * FROM product;
+INSERT INTO product (name, price, occation_id, item_type_id) VALUES ('Helium Ballon', 12.99, (SELECT occation_id FROM occation WHERE occation_name = 'Birthday'), (SELECT item_type_id FROM item_type WHERE item_name = 'Ballon'));
+
+INSERT INTO product (name, price, occation_id, item_type_id) VALUES ('Roses', 3.99, (SELECT occation_id FROM occation WHERE occation_name = 'Anniversary'), (SELECT item_type_id FROM item_type WHERE item_name = 'Flower'));
+
+INSERT INTO product (name, price, occation_id, item_type_id) VALUES ('Roses with Gypsophila', 19.99, (SELECT occation_id FROM occation WHERE occation_name = 'Anniversary'), (SELECT item_type_id FROM item_type WHERE item_name = 'Valintine\'s Bouquet'));
+
+
+
+SELECT * FROM inventory;
+INSERT INTO inventory (store_id, product_id, quantity) VALUES ((SELECT store_id FROM store s INNER JOIN address a ON a.address_id = s.store_id INNER JOIN state st ON st.state_id = a.state_id WHERE state_abv = 'PA'), (SELECT product_id FROM product WHERE name = 'Helium Ballon'), 5);
+
+INSERT INTO inventory (store_id, product_id, quantity) VALUES ((SELECT store_id FROM store s INNER JOIN address a ON a.address_id = s.store_id INNER JOIN state st ON st.state_id = a.state_id WHERE state_abv = 'ID'), (SELECT product_id FROM product WHERE name = 'Roses'), 10);
+
+INSERT INTO inventory (store_id, product_id, quantity) VALUES ((SELECT store_id FROM store s INNER JOIN address a ON a.address_id = s.store_id INNER JOIN state st ON st.state_id = a.state_id WHERE state_abv = 'AZ'), (SELECT product_id FROM product WHERE name = 'Roses with Gypsophila'), 3);
+
+
+
+SELECT * FROM flower_shop.order;
+INSERT INTO flower_shop.order (store_id, employee_id, customer_id, date, total) VALUES ((SELECT store_id FROM store s INNER JOIN address a ON a.address_id = s.address_id WHERE city = 'Rexburgh'), (SELECT employee_id FROM employee WHERE first_name = 'Heather' and last_name = 'Washington'), (SELECT customer_id FROM customer WHERE first_name = 'Emily' and last_name = 'Levy'), '2024-01-13', 100.00);
+
+INSERT INTO flower_shop.order (store_id, employee_id, customer_id, date, total) VALUES ((SELECT store_id FROM store s INNER JOIN address a ON a.address_id = s.address_id WHERE city = 'Phoenix'), (SELECT employee_id FROM employee WHERE first_name = 'Alex' and last_name = 'Jackson'), (SELECT customer_id FROM customer WHERE first_name = 'Douglas' and last_name = 'Young'), '2024-01-14', 100.00);
+
+INSERT INTO flower_shop.order (store_id, employee_id, customer_id, date, total) VALUES ((SELECT store_id FROM store s INNER JOIN address a ON a.address_id = s.address_id WHERE city = 'Pittsburgh'), (SELECT employee_id FROM employee WHERE first_name = 'Jason' and last_name = 'Murray'), (SELECT customer_id FROM customer WHERE first_name = 'Cesar' and last_name = 'Moyer'), '2024-01-15', 100.00);
+
+
+
+SELECT * FROM order_line;
+INSERT INTO order_line (order_id, product_id) VALUES ((SELECT order_id FROM flower_shop.order WHERE customer_id = (SELECT customer_id FROM customer WHERE first_name = 'Emily' and last_name = 'Levy')), (SELECT product_id FROM product WHERE name = 'Roses'));
+
+INSERT INTO order_line (order_id, product_id) VALUES ((SELECT order_id FROM flower_shop.order WHERE customer_id = (SELECT customer_id FROM customer WHERE first_name = 'Douglas' and last_name = 'Young')), (SELECT product_id FROM product WHERE name = 'Roses with Gypsophila'));
+
+INSERT INTO order_line (order_id, product_id) VALUES ((SELECT order_id FROM flower_shop.order WHERE customer_id = (SELECT customer_id FROM customer WHERE first_name = 'Cesar' and last_name = 'Moyer')), (SELECT product_id FROM product WHERE name = 'Helium Ballon'));
+
+
+
+
+SELECT * FROM truck;
+INSERT INTO truck (license_plate, make, model) VALUES ('WZB-123', 'Zephyr', 'Stellar Cruiser');
+
+INSERT INTO truck (license_plate, make, model) VALUES ('YGT-789', 'Quasar', 'Quantum Velocity');
+
+INSERT INTO truck (license_plate, make, model) VALUES ('KLM-456', 'Nebula', 'Nimbus X');
+
+
+
+
+SELECT * FROM delivery;
+INSERT INTO delivery (order_id, truck_id) VALUES ((SELECT order_id FROM flower_shop.order WHERE customer_id = (SELECT customer_id FROM customer WHERE first_name = 'Emily' and last_name = 'Levy')), (SELECT truck_id FROM truck WHERE license_plate = 'WZB-123'));
+
+INSERT INTO delivery (order_id, truck_id) VALUES ((SELECT order_id FROM flower_shop.order WHERE customer_id = (SELECT customer_id FROM customer WHERE first_name = 'Douglas' and last_name = 'Young')), (SELECT truck_id FROM truck WHERE license_plate = 'YGT-789'));
+
+INSERT INTO delivery (order_id, truck_id) VALUES ((SELECT order_id FROM flower_shop.order WHERE customer_id = (SELECT customer_id FROM customer WHERE first_name = 'Cesar' and last_name = 'Moyer')), (SELECT truck_id FROM truck WHERE license_plate = 'KLM-456'));
